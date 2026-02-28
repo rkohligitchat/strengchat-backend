e471e49c624da8850c10a9fae6fd263c82b60662
rkohligitchat <rkohli0005@gmail.com>
2026-02-28 15:43:34 +0530

    updated

--- db.js
+++ db.js
@@ -1,15 +1,7 @@
 // db.js
 const mysql = require("mysql2/promise");
 
-const db = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  port: process.env.MYSQLPORT,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  ssl: {
-    rejectUnauthorized: false
-  } // ⭐ VERY IMPORTANT for Railway MySQL
-});
+const db = mysql.createPool(process.env.DATABASE_URL);
+
 
 module.exports = db;
